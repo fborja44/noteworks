8012e87daef20e8862b38c2e618096c98b2efa7b
Francis Borja <fborja4400@gmail.com>
2021-06-28 21:25:37 -0400

    refactor: define style variables

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,17 @@
-import {RiSettings5Fill, RiMenuLine} from "react-icons/ri";
+import { RiSettings5Fill, RiMenuLine } from "react-icons/ri";
+import { BiLogIn } from "react-icons/bi";
 
 const NavBar = () => {
     return (
         <nav id="header-nav">
-            <button className="header-button login-button">Login</button>
-            <button className="header-button signup-button">Sign-Up</button>
-            <RiMenuLine className="header-icon menu-icon" />
-            <RiSettings5Fill className="header-icon settings-icon" />
+            <button className="header-button login-button header-nav-element">
+                <span>Login</span><BiLogIn className="login-icon" />
+            </button>
+            <button className="header-button signup-button header-nav-element">
+                <span>Sign-Up</span>
+            </button>
+            <RiMenuLine className="header-icon menu-icon header-nav-element" />
+            <RiSettings5Fill className="header-icon settings-icon header-nav-element" />
         </nav>
     );
 };
