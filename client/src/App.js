e8966ec05412cf06c6dd9e2d8c60f9303005a04f
Francis Borja <fborja4400@gmail.com>
2021-07-05 11:06:14 -0400

    feat: reinitialize project using electron

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+/* Main App Component
+------------------------------------------------------------------------------*/
+// Component imports
+
+// CSS imports
+
+function App() {
+  return (
+      <div className="App">
+        
+      </div>
+  );
+}
+
+export default App;
