578c5a91b17ceb4860e5c25f5f6759ba87a4e3c5
Francis Borja <fborja4400@gmail.com>
2021-07-07 01:20:22 -0400

    feat: change color on menu selection

--- client/src/components/quicknotes/ColorMenu.jsx
+++ client/src/components/quicknotes/ColorMenu.jsx
@@ -1,4 +1,8 @@
+// React imports
 import React, { useRef, useEffect, useCallback } from "react";
+
+// Common imports
+import { COLOR } from "../../common/color";
 
 // Image and icon imports
 import { MdClose } from "react-icons/md";
@@ -32,6 +36,11 @@ const ColorMenu = ({ showColorMenu, setShowColorMenu, setLabelColor }) => {
     setShowColorMenu(false);
   };
 
+  // Option on hover: Display checkmark
+  const handleMouseHover = (event) => {
+    event.target.style.display = "block";
+  };
+
   return (
     <React.Fragment>
       {/* Ternary operator to determine whether to render modal */}
@@ -47,14 +56,46 @@ const ColorMenu = ({ showColorMenu, setShowColorMenu, setLabelColor }) => {
               />
             </div>
             <div className="color-menu-content">
-              <div onClick={handleOnClick} data-color="RED" className="color-option red"></div>
-              <div onClick={handleOnClick} data-color="ORANGE " className="color-option orange"></div>
-              <div onClick={handleOnClick} data-color="YELLOW" className="color-option yellow"></div>
-              <div onClick={handleOnClick} data-color="GREEN" className="color-option green"></div>
-              <div onClick={handleOnClick} data-color="BLUE" className="color-option blue"></div>
-              <div onClick={handleOnClick} data-color="PURPLE" className="color-option purple"></div>
-              <div onClick={handleOnClick} data-color="PINK" className="color-option pink"></div>
-              <div onClick={handleOnClick} data-color="CYAN" className="color-option cyan"></div>
+              <div
+                onClick={handleOnClick}
+                data-color={COLOR.RED}
+                className="color-option red"
+              ></div>
+              <div
+                onClick={handleOnClick}
+                data-color={COLOR.ORANGE}
+                className="color-option orange"
+              ></div>
+              <div
+                onClick={handleOnClick}
+                data-color={COLOR.YELLOW}
+                className="color-option yellow"
+              ></div>
+              <div
+                onClick={handleOnClick}
+                data-color={COLOR.GREEN}
+                className="color-option green"
+              ></div>
+              <div
+                onClick={handleOnClick}
+                data-color={COLOR.BLUE}
+                className="color-option blue"
+              ></div>
+              <div
+                onClick={handleOnClick}
+                data-color={COLOR.PURPLE}
+                className="color-option purple"
+              ></div>
+              <div
+                onClick={handleOnClick}
+                data-color={COLOR.PINK}
+                className="color-option pink"
+              ></div>
+              <div
+                onClick={handleOnClick}
+                data-color={COLOR.CYAN}
+                className="color-option cyan"
+              ></div>
             </div>
           </section>
         </div>
